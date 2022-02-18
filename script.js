41c5288ce21223737690a2e2f93ce5721b0f25ab
Mads-SJ <mads.soenderstrup@gmail.com>
2022-02-18 10:13:18 +0100

    navbar with name and icons

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+document.getElementById("linked-in").onclick = function () {
+  window.open(
+    "https://www.linkedin.com/in/mads-s%C3%B8nderstrup-jensen-025309225/",
+    "_blank"
+  );
+};
+
+document.getElementById("github").onclick = function () {
+  window.open("https://github.com/Mads-SJ", "_blank");
+};
+
 document.getElementById("reaction-time-btn").onclick = function () {
   window.open("/reaction-time-app", "_blank");
 };
