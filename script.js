bfef511caadd1084b5c92e855869765ec62a244d
Mads <mads.soenderstrup@gmail.com>
2022-03-07 09:37:07 +0100

    routing for countdown

--- script.js
+++ script.js
@@ -14,7 +14,9 @@ document.getElementById("reaction-time-btn").onclick = function () {
 };
 
 document.getElementById("weather-app-btn").onclick = function () {
-  window.open("/weather-app", "_blank");
+  alert(
+    "This app had to be taken down to hide the API key. The API key has been revoked. Please take a look at the code instead."
+  );
 };
 
 document.getElementById("todo-app-btn").onclick = function () {
@@ -23,6 +25,10 @@ document.getElementById("todo-app-btn").onclick = function () {
 
 document.getElementById("tip-calculator-btn").onclick = function () {
   window.open("/tip-calculator", "_blank");
+};
+
+document.getElementById("countdown-btn").onclick = function () {
+  window.open("/countdown", "_blank");
 };
 
 document.getElementById("portfolio-github-btn").onclick = function () {
@@ -43,4 +49,8 @@ document.getElementById("todo-app-github-btn").onclick = function () {
 
 document.getElementById("tip-calculator-github-btn").onclick = function () {
   window.open("https://github.com/Mads-SJ/tip-calculator", "_blank");
+};
+
+document.getElementById("countdown-github-btn").onclick = function () {
+  window.open("https://github.com/Mads-SJ/birthday-countdown", "_blank");
 };
