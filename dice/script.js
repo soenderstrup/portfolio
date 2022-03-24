34e15070cfd41747105e4a1e1022c58bf743edeb
Mads-SJ <mads.soenderstrup@gmail.com>
2022-03-24 09:30:34 +0100

    refactored script

--- dice/script.js
+++ dice/script.js
@@ -1,109 +1,97 @@
 class Dice {
   constructor() {
-    const diceElement = this.createDiceElement();
-    this.element = diceElement;
-    diceContainer.appendChild(diceElement);
+    this.diceElement = this.createDiceElement();
 
-    this.element.addEventListener("animationend", () => {
-      this.element.classList.remove("rolling");
+    this.diceElement.addEventListener("animationend", () => {
+      this.diceElement.classList.remove("rolling");
     });
 
-    this.element.addEventListener("click", () => {
+    this.diceElement.addEventListener("click", () => {
       this.roll();
     });
-  }
-
-  roll() {
-    if (this.element.classList.contains("rolling")) return;
-
-    this.updateFace();
-    this.element.classList.add("rolling");
   }
 
   createDiceElement() {
     const diceElement = document.createElement("div");
     diceElement.classList.add("dice");
-    diceElement.appendChild(this.createFace(1));
+    diceElement.innerHTML = this.getNewEyes(1);
+    diceContainer.appendChild(diceElement);
     return diceElement;
   }
 
-  updateFace() {
-    const oldFace = this.element.firstChild;
-    const newFace = this.createFace(this.randomNumber(6));
-    this.element.replaceChild(newFace, oldFace);
+  roll() {
+    if (this.diceElement.classList.contains("rolling")) return;
+
+    this.diceElement.innerHTML = this.getNewEyes(this.randomNumber(6));
+    this.diceElement.classList.add("rolling");
   }
 
-  createFace(numberOfEyes) {
-    let face = document.createElement("div");
-    face.classList.add("face");
+  getNewEyes(numberOfEyes) {
+    let face;
+    let dot = `<span class="dot"></span>`;
 
     switch (numberOfEyes) {
       case 1:
-        face.classList.add("first-face");
-        face.appendChild(this.createDot());
+        face = `<div class="face first-face">
+                  ${dot}
+                </div>`;
         break;
       case 2:
-        face.classList.add("second-face");
-        for (let i = 0; i < 2; i++) {
-          face.appendChild(this.createDot());
-        }
+        face = `<div class="face second-face">
+                  ${dot}
+                  ${dot}
+                </div>`;
         break;
       case 3:
-        face.classList.add("third-face");
-        for (let i = 0; i < 3; i++) {
-          face.appendChild(this.createDot());
-        }
+        face = `<div class="face third-face">
+                  ${dot}
+                  ${dot}
+                  ${dot}
+                </div>`;
         break;
       case 4:
-        face.classList.add("fourth-face");
-        for (let i = 0; i < 2; i++) {
-          const column = this.createDotColumn();
-          for (let j = 0; j < 2; j++) {
-            column.appendChild(this.createDot());
-          }
-          face.appendChild(column);
-        }
+        face = `<div class="face fourth-face">
+                  <div class="column">
+                    ${dot}
+                    ${dot}
+                  </div>
+                  <div class="column">
+                    ${dot}
+                    ${dot}
+                  </div>
+                </div>`;
         break;
       case 5:
-        face.classList.add("fifth-face");
-        for (let i = 0; i < 2; i++) {
-          const column = this.createDotColumn();
-
-          if (i === 1) {
-            const column = this.createDotColumn();
-            column.appendChild(this.createDot());
-            face.appendChild(column);
-          }
-          for (let j = 0; j < 2; j++) {
-            column.appendChild(this.createDot());
-          }
-          face.appendChild(column);
-        }
+        face = `<div class="face fifth-face">
+          	      <div class="column">
+                    ${dot}
+                    ${dot}
+                  </div>
+                  <div class="column">
+                    ${dot}
+                  </div>
+                  <div class="column">
+                    ${dot}
+                    ${dot}
+                  </div>
+                </div>`;
         break;
       case 6:
-        face.classList.add("sixth-face");
-        for (let i = 0; i < 2; i++) {
-          const column = this.createDotColumn();
-          for (let j = 0; j < 3; j++) {
-            column.appendChild(this.createDot());
-          }
-          face.appendChild(column);
-        }
+        face = `<div class="face sixth-face">
+                  <div class="column">
+                    ${dot}
+                    ${dot}
+                    ${dot}
+                  </div>
+                  <div class="column">
+                    ${dot}
+                    ${dot}
+                    ${dot}
+                  </div>
+                </div>`;
         break;
     }
     return face;
-  }
-
-  createDotColumn() {
-    const column = document.createElement("div");
-    column.classList.add("column");
-    return column;
-  }
-
-  createDot() {
-    const dot = document.createElement("span");
-    dot.classList.add("dot");
-    return dot;
   }
 
   randomNumber(max) {
@@ -116,7 +104,7 @@ const diceContainer = document.getElementById("dice-container");
 const rollBtn = document.getElementById("roll-btn");
 let dices = [];
 
-function createNumberOfDices(noOfDices) {
+function createDices(noOfDices) {
   for (let i = 0; i < noOfDices; i++) {
     let dice = new Dice();
     dices.push(dice);
@@ -131,7 +119,7 @@ function removeDices() {
 selector.addEventListener("change", () => {
   const noOfDices = selector.options[selector.selectedIndex].value;
   removeDices();
-  createNumberOfDices(noOfDices);
+  createDices(noOfDices);
 });
 
 rollBtn.addEventListener("click", () => {
@@ -140,4 +128,4 @@ rollBtn.addEventListener("click", () => {
   });
 });
 
-createNumberOfDices(1);
+createDices(1);
